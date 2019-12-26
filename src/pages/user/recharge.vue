44eb53c401bbeb35a8aa318a062674c8d24fef26
stavyan <617946852@qq.com>
2019-12-26 21:52:26 +0800

    调试微信h5支付

--- src/pages/user/recharge.vue
+++ src/pages/user/recharge.vue
@@ -148,24 +148,18 @@
 					 data: JSON.stringify(params)
 				 }).then(r => {
 					 if (r.code === 200) {
-						 console.log(r.data)
-						 jweixin.config({
-							 debug: true,
-					　　　　　　appId: 'wx869d264c83ad71cc', // 必填，公众号的唯一标识
-					　　　　　　timestamp: r.data.config.mch_id, // 必填，生成签名的时间戳
-					　　　　　　nonceStr: r.data.config.nonce_str, // 必填，生成签名的随机串
-					　　　　　　signature: r.data.config.sign,// 必填，签名，见附录1
-							 jsApiList: [
-				　　　　　　　　	"openLocation"
-					　　　　　　]
+						 jweixin.ready(()=>{
+							 jweixin.chooseWXPay({
+								 timestamp: r.data.config.mch_id, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+								 nonceStr: r.data.config.nonce_str, // 支付签名随机串，不长于 32 位
+								 package: `prepay_id=${r.data.config.prepay_id}`, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
+								 signType: 'MD5', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+								 paySign: r.data.config.sign, // 支付签名
+								 success: function (res) {
+									 // 支付成功后的回调函数
+								 }
+							 });
 						 })
-// 						 appId: datad.appid, // 必填，公众号的唯一标识
-// 　　　　　　timestamp: datad.timestamp, // 必填，生成签名的时间戳
-// 　　　　　　nonceStr: datad.noncestr, // 必填，生成签名的随机串
-// 　　　　　　signature: datad.signature,// 必填，签名，见附录1
-// 　　　　　　jsApiList: [
-// 　　　　　　　　"openLocation"
-// 　　　　　　] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
 					 } else {
 						 uni.showToast({title: r.message, icon: "none"});
 					 }
