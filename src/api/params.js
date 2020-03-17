244cee73d57071ce4f512fb301510a93ffceb02e
stavyan <617946852@qq.com>
2020-03-17 09:32:37 +0800

    add base branch

--- src/api/params.js
+++ src/api/params.js
@@ -1,11 +1,11 @@
 // 主机地址
-const hostUrl = "http://html5.tinyshop.yllook.com/";
+const hostUrl = "http://h5.tinyshop.rageframe.com";
 
 // 充值页面地址 用作微信H5获取code
 const rechargeUrl = `${hostUrl}/pages/user/recharge`;
 
 // 后台接口请求地址
-const baseUrl = "https://www.yllook.com/api";
+const baseUrl = "http://demo.rageframe.com/api";
 
 const weixinAppId = "";
 const qqAppId = "";
