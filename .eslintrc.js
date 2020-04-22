4a4574613ec3d7b7bd3a60058ed598d2f4f824d4
luoxiaochuan <luoxiaochuan@comteck.cn>
2020-04-22 18:08:27 +0800

    chore: eslint 添加 uni 为全局变量

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,8 @@ module.exports = {
     ],
     "globals": {
         "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+				"SharedArrayBuffer": "readonly",
+				uni: true,
     },
     "parserOptions": {
         "ecmaVersion": 2018,
