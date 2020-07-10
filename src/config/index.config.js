700a0ef9c60df829a78f9860308633fa8a1d73cc
stavyan <617946852@qq.com>
2020-07-10 13:45:13 +0800

    更换：后端api地址

--- src/config/index.config.js
+++ src/config/index.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
     // 开发环境配置
     development: {
         assetsPath: '/static', // 静态资源路径
-				baseUrl: 'http://b2c.rageframe.com/api', // 后台接口请求地址
+				baseUrl: 'http://demo.rageframe.com/api', // 后台接口请求地址
 				hostUrl: 'http://h5.tinyshop.rageframe.com', // H5地址(前端运行地址)
         websocketUrl: '', // websocket服务端地址
         weixinAppId: '' // 微信公众号appid
@@ -10,7 +10,7 @@ const CONFIG = {
     // 生产环境配置
     production: {
         assetsPath: '/static', // 静态资源路径
-        baseUrl: 'http://b2c.rageframe.com/api', // 后台接口请求地址
+        baseUrl: 'http://demo.rageframe.com/api', // 后台接口请求地址
         hostUrl: 'http://h5.tinyshop.rageframe.com', // H5地址(前端运行地址)
         websocketUrl: '', // websocket服务端地址
         weixinAppId: '' // 微信公众号appid
