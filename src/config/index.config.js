5165ecbb768b8534b26ab0704bdaa148fe3c7934
stavyan <617946852@qq.com>
2020-07-21 21:43:40 +0800

    修复：预售订单干扰价格显示

--- src/config/index.config.js
+++ src/config/index.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
     // 开发环境配置
     development: {
         assetsPath: '/static', // 静态资源路径
-				baseUrl: 'http://app.gyptbbs.com/api', // 后台接口请求地址
+				baseUrl: 'http://demo.rageframe.com/api', // 后台接口请求地址
 				hostUrl: 'http://h5.tinyshop.rageframe.com', // H5地址(前端运行地址)
         websocketUrl: '', // websocket服务端地址
         weixinAppId: '' // 微信公众号appid
